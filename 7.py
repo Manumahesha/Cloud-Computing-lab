e9c7b9c7947666d1d52380ff9fa582dd7658edc7
Mahadevaprasad <129720075+Manumahesha@users.noreply.github.com>
2024-11-21 15:19:09 +0530

    Update 7.py

--- 7.py
+++ 7.py
@@ -26,3 +26,14 @@ try:
     print("Signature is valid")
 except:
     print("Signature verification failed.")
+
+output
+Public key in PEM format:
+ -----BEGIN PUBLIC KEY-----
+MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEy+/Lu6VVA0YwYSeCXGvdKBixhMoF
+Tp09jT7mRwS/vGppKJyMWTgQFcMFcB7qAJ1YGJS7OBE9PM/wsVwJix7npg==
+-----END PUBLIC KEY-----
+
+Enter a message to be signed: Hello
+Signature: b'0E\x02!\x00\x83#h\xd9D\xcc\x18\x8bX\xcf\xd7\x1a\x00H\r^\x0eh\xd9[\xa7{\xec\r*\xec\x7f\xd5\xab\x88\xe3\xcc\x02 3!a\xf6To\xb5\x99J5\xaa\x92\xb4\x9d\x08\x1e\xc4B3H\xa4\x16}\xa0\xe3\xbb\x84#\x8a\xa6X\x9b'
+Signature is valid
